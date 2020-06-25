a151d54ee703aae32627fe0900ee49af295a4c64
marmoulman <38055265+marmoulman@users.noreply.github.com>
2020-06-25 12:59:24 -0400

    Add project files.

---
+++ hw-art-of-war/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hw_art_of_war
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
